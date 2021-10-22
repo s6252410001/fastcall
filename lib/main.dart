97fb1039f2b38b1690a8fa35d4624f18f7d20aaa
s6252410001 <s6252410001@sau.ac.th>
2021-10-22 12:32:50 +0700

    Code V.1

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_fastcall/views/listfood_new_ui.dart';
+import 'package:flutter_app_fastcall/views/listfood_ui.dart';
+
+main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ListfoodNewUI(),
+      theme: ThemeData(fontFamily: 'Kanit'),
+    ),
+  );
+}
